c5fb578daf286c54e51b765b36fe3608aa93c02e
DhavalKumar7 <dk4722664@gmail.com>
2025-09-18 18:35:04 +0530

    sitemap.js

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -1,13 +1,24 @@
 export default function handler(req, res) {
   res.setHeader("Content-Type", "application/xml");
 
-  // Example sitemap, you can add more URLs here
+  const baseUrl = "https://dhavaflix.vercel.app";
+
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
-      <loc>https://dhavaflix.vercel.app/</loc>
-      <lastmod>${new Date().toISOString()}</lastmod>
+      <loc>${baseUrl}/</loc>
+      <changefreq>daily</changefreq>
       <priority>1.0</priority>
+    </url>
+    <url>
+      <loc>${baseUrl}/movies</loc>
+      <changefreq>weekly</changefreq>
+      <priority>0.8</priority>
+    </url>
+    <url>
+      <loc>${baseUrl}/tvshows</loc>
+      <changefreq>weekly</changefreq>
+      <priority>0.8</priority>
     </url>
   </urlset>`;
 
