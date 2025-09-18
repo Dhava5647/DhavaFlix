9584f0d93b0be5d48405d039dd56ba5d1c9c4100
DhavalKumar7 <dk4722664@gmail.com>
2025-09-18 21:02:09 +0530

    movies.js

---
+++ Api/movies.js
@@ -0,0 +1,12 @@
+// pages/api/movies.js
+export default async function handler(req, res) {
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
+    );
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch movies" });
+  }
+}
