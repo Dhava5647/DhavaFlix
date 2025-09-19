595c6e604cf74f52b084d13c4eb07aa0c59f34e1
DhavalKumar7 <dk4722664@gmail.com>
2025-09-19 08:51:10 +0530

    tv.js

--- Api/tv.js
+++ Api/tv.js
@@ -2,11 +2,12 @@
 export default async function handler(req, res) {
   try {
     const response = await fetch(
-      `https://api.themoviedb.org/3/tv/popular?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
+      `https://api.themoviedb.org/3/tv/popular?api_key=0627e9682f6c3eca80da4e2a6217ce57&language=en-US&page=1`
     );
     const data = await response.json();
     res.status(200).json(data);
   } catch (error) {
+    console.error(error);
     res.status(500).json({ error: "Failed to fetch TV shows" });
   }
 }
