30cc8edb15962f8404fc8e081c29d9c0390c786c
DhavalKumar7 <dk4722664@gmail.com>
2025-09-19 13:28:27 +0530

    search.js

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,20 +1,9 @@
-// pages/api/search.js
 export default async function handler(req, res) {
-  const { query } = req.query; // Example: /api/search?query=batman
-
-  if (!query) {
-    return res.status(400).json({ error: "Missing search query" });
-  }
-
   try {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/search/multi?api_key=${process.env.TMDB_API_KEY}&language=en-US&query=${encodeURIComponent(
-        query
-      )}&page=1&include_adult=false`
-    );
-
-    const data = await response.json();
+    const data = { message: "search works!" }; // temporary response
     res.status(200).json(data);
   } catch (error) {
     console.error(error);
-    res.status(500
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
