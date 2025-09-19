e2044875b45f78558eec8b6b902c2c85cea11beb
DhavalKumar7 <dk4722664@gmail.com>
2025-09-19 13:44:03 +0530

    _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '@/style.css'   // import your global CSS
+import '../style.css'   // go up one folder and load style.css
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
