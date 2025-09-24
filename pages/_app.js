1e59ab41c4e8ecb369a0e263dcf5e06a6c74ff3e
Dhava5647 <butlegar814@gmail.com>
2025-09-24 07:35:25 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
-import '../style.css'   // go up one folder and load style.css
+import '../styles/globals.css';
+import { AnimatePresence, motion } from 'framer-motion';
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// This component wraps your entire application, enabling animations between pages.
+function MyApp({ Component, pageProps, router }) {
+  return (
+    <AnimatePresence exitBeforeEnter>
+      <motion.div
+        key={router.route}
+        initial={{ opacity: 0, y: 15 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -15 }}
+        transition={{ duration: 0.25 }}
+      >
+        <Component {...pageProps} />
+      </motion.div>
+    </AnimatePresence>
+  );
 }
+
+export default MyApp;
